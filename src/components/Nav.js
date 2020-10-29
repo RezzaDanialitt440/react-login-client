349deb626143bbe28f522535c1acb53ab9a475b8
Rezza Danial <rezzadanialcs@gmail.com>
2020-10-30 00:19:19 +0800

    finished registration page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {  Container, AppBar, Toolbar, Button, Typography  } from '@material-ui/core'
+import {  AppBar, Toolbar, Button, Typography  } from '@material-ui/core'
 
 const Nav = () => {
 
