d7b3cd0d037e28007c3ec39cddbd82a2e4a7eefd
Rezza Danial <rezzadanialcs@gmail.com>
2020-11-01 16:39:21 +0800

    improve from validation

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -46,7 +46,6 @@ const Register = ({setAlert, register, isAuthenticated}) => {
             name="name"
             value={name}
             onChange={e => onChange(e)}
-            required
           />
         </div>
         <div className="form-group">
@@ -56,7 +55,6 @@ const Register = ({setAlert, register, isAuthenticated}) => {
             name="email"
             value={email}
             onChange={e => onChange(e)}
-            required
           />
         </div>
         <div className="form-group">
@@ -67,7 +65,6 @@ const Register = ({setAlert, register, isAuthenticated}) => {
             minLength="12"
             value={password}
             onChange={e => onChange(e)}
-            required
           />
         </div>
         <div className="form-group">
@@ -78,7 +75,6 @@ const Register = ({setAlert, register, isAuthenticated}) => {
             minLength="12"
             value={password2}
             onChange={e => onChange(e)}
-            required
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
